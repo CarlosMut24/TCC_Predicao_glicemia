41ae076057dd515976f58255a5be8dcd7cabf485
CarlosMut24 <carloshfunchal@gmail.com>
2026-07-27 15:31:31 -0300

    aprendo a ler o XML

--- Scripts/Importar.py
+++ Scripts/Importar.py
@@ -1,17 +1,28 @@
 import os
+import xml.dom.minidom 
 import xml.etree.ElementTree as Et
 
-class Read_xml():
-    def __init__(self, directoty) -> None:
-        self.directoty = directoty
+def get_xml_path():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))  
+    return os.path.join(BASE_DIR, "..", "OhioT1DM", "2018", "test", "559-ws-testing.xml")
 
-    def all_files(self):
-        return [ os.path.join(self.directoty, arq) for arq in os.listdir(self.directoty)
-        if arq.lower().endswith(".xml")]
+domtree = xml.dom.minidom.parse(get_xml_path())
 
-    def nfe_data(self, xml):
-        root = Et.parse(xml).getroot()
-        ns = {"ns": "patient"}
+patient = domtree.documentElement
+assert patient is not None
+print(f"--- Patient {patient.getAttribute('id')} ---")
 
-    def check_none(self, var):
-        pass
+glucose_level = patient.getElementsByTagName('glucose_level')[0].getElementsByTagName('event')
+
+parar = 0
+
+print("-- Nivel de glicose --")
+for event in glucose_level:
+
+    print (f"tempo: {event.getAttribute('ts')} glicose: {event.getAttribute('value')}")
+
+    parar = parar + 1
+    if parar >= 4 :
+        parar = 0
+        break
+
