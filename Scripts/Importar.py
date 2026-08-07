7ae9a1446c20135355cbdf38571c488acce2027d
CarlosMut24 <carloshfunchal@gmail.com>
2026-08-07 15:35:33 -0300

    aprendo o pandas

--- Scripts/Importar.py
+++ Scripts/Importar.py
@@ -1,12 +1,17 @@
 import os
 import math
+import pandas as pd
+import numpy as np
 from pathlib import Path
 import xml.dom.minidom 
 from datetime import datetime
 import xml.etree.ElementTree as Et
 
 def get_xml_root():
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))  
+    try:
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    except NameError:
+        BASE_DIR = os.getcwd()
     return os.path.join(BASE_DIR, "..", "OhioT1DM")
 
 def get_XMLs(root):
@@ -32,9 +37,6 @@ def get_info(file_XML, dados: dict):
     assert patient is not None
 
     id_patient = int(patient.getAttribute('id'))
-
-    if id_patient not in dados.keys():
-        dados[id_patient] = {}
         
 
     parar = 0 #serve para os lupes não imundar o painel de comando, sera remorido depis
@@ -50,7 +52,18 @@ def get_info(file_XML, dados: dict):
 
     for event in finger_stick:
         ts =  binning(event.getAttribute('ts'))
-        dados[id_patient][ts] = {"medidor": "paciente", "glucose_level": int(event.getAttribute('value'))}
+        dados[id_patient][ts] = {
+                                    "medidor": "paciente", 
+                                 "glucose_level": int(event.getAttribute('value')),
+                                 "basal": None,
+                                 "bolus": None,
+                                 "bolus_bwz_carb_input": None,
+                                 "meal_type": None, 
+                                 "meal_carbs": None,
+                                 "sleep": None,
+                                 "sleep_quality": None,
+                                 "exercise_intensity": None,
+                                 "glucose_level": None}
 
     for event in glucose_level:
         ts =  binning(event.getAttribute('ts'))
@@ -58,15 +71,34 @@ def get_info(file_XML, dados: dict):
             dados[id_patient][ts]["medidor"] = "CGM"
             dados[id_patient][ts]["glucose_level"] = int(event.getAttribute('value'))           
         except:
-            dados[id_patient][ts] = {"medidor": "CGM", "glucose_level": int(event.getAttribute('value'))}    
+            dados[id_patient][ts] = {"medidor": "CGM", 
+                                     "glucose_level": int(event.getAttribute('value')),
+                                     "basal": None,
+                                     "bolus": None,
+                                     "bolus_bwz_carb_input": None,
+                                     "meal_type": None, 
+                                     "meal_carbs": None,
+                                     "sleep": None,
+                                     "sleep_quality": None,
+                                     "exercise_intensity": None,
+                                     "glucose_level": None}
 
     for event in basal:
         ts =  binning(event.getAttribute('ts'))
         try:
             dados[id_patient][ts]["basal"] = float(event.getAttribute('value'))
         except:
-            dados[id_patient][ts] = {"basal": float(event.getAttribute('value')),
-                                    "glucose_level": 0, "medidor": "null"}
+            dados[id_patient][ts] = {"medidor": None, 
+                                     "glucose_level": None,
+                                     "basal": float(event.getAttribute('value')),
+                                     "bolus": None,
+                                     "bolus_bwz_carb_input": None,
+                                     "meal_type": None, 
+                                     "meal_carbs": None,
+                                     "sleep": None,
+                                     "sleep_quality": None,
+                                     "exercise_intensity": None,
+                                     "glucose_level": None}
 
     for event in bolus:
         ts =  binning(event.getAttribute('ts_begin'))
@@ -74,8 +106,17 @@ def get_info(file_XML, dados: dict):
             dados[id_patient][ts]["bolus"] = event.getAttribute('dose')
             dados[id_patient][ts]["bolus_bwz_carb_input"] = event.getAttribute('bwz_carb_input')
         except:
-            dados[id_patient][ts] = {"bolus": event.getAttribute('dose'), "bolus_bwz_carb_input": event.getAttribute('bwz_carb_input'), 
-                                    "glucose_level": 0, "medidor": "null"}
+            dados[id_patient][ts] = {"medidor": None, 
+                                     "glucose_level": None,
+                                     "basal": None,
+                                     "bolus": event.getAttribute('dose'),
+                                     "bolus_bwz_carb_input": event.getAttribute('bwz_carb_input'),
+                                     "meal_type": None, 
+                                     "meal_carbs": None,
+                                     "sleep": None,
+                                     "sleep_quality": None,
+                                     "exercise_intensity": None,
+                                     "glucose_level": None}
 
     for event in meal:
         ts =  binning(event.getAttribute('ts'))
@@ -83,8 +124,17 @@ def get_info(file_XML, dados: dict):
             dados[id_patient][ts]["meal_type"] = event.getAttribute('type')
             dados[id_patient][ts]["meal_carbs"] = event.getAttribute('carbs')
         except:
-            dados[id_patient][ts] = {"meal_type": event.getAttribute('type'),"meal_carbs": event.getAttribute('carbs'),
-                                     "glucose_level": 0, "medidor": "null"}
+            dados[id_patient][ts] = {"medidor": None, 
+                                     "glucose_level": None,
+                                     "basal": None,
+                                     "bolus": None,
+                                     "bolus_bwz_carb_input": None,
+                                     "meal_type": event.getAttribute('type'), 
+                                     "meal_carbs": event.getAttribute('carbs'),
+                                     "sleep": None,
+                                     "sleep_quality": None,
+                                     "exercise_intensity": None,
+                                     "glucose_level": None}
 
     for event in sleep:
         ts_begin =  binning(event.getAttribute('ts_begin'))
@@ -93,15 +143,32 @@ def get_info(file_XML, dados: dict):
             dados[id_patient][ts_begin]["sleep"] = 'comeso'
             dados[id_patient][ts_begin]["sleep_quality"] = event.getAttribute('quality')
         except:
-            dados[id_patient][ts_begin] = {"sleep": 'comeso', "sleep_quality": event.getAttribute('quality'),
-                                            "glucose_level": 0, "medidor": "null"}
-
+            dados[id_patient][ts_begin] = {"medidor": None, 
+                                             "glucose_level": None,
+                                             "basal": None,
+                                             "bolus": None,
+                                             "bolus_bwz_carb_input": None,
+                                             "meal_type": None,
+                                             "meal_carbs": None,
+                                             "sleep": 'comeso',
+                                             "sleep_quality": event.getAttribute('quality'),
+                                             "exercise_intensity": None,
+                                             "glucose_level": None}
         try:
             dados[id_patient][ts_end]["sleep"] = 'fim'
             dados[id_patient][ts_end]["sleep_quality"] = event.getAttribute('quality')
         except:
-            dados[id_patient][ts_end] = {"sleep": 'fim', "sleep_quality": event.getAttribute('quality'),
-                                        "glucose_level": 0, "medidor": "null"}
+            dados[id_patient][ts_end] = {"medidor": None, 
+                                             "glucose_level": None,
+                                             "basal": None,
+                                             "bolus": None,
+                                             "bolus_bwz_carb_input": None,
+                                             "meal_type": None,
+                                             "meal_carbs": None,
+                                             "sleep": 'fim',
+                                             "sleep_quality": event.getAttribute('quality'),
+                                             "exercise_intensity": None,
+                                             "glucose_level": None}
 
     for event in exercise:
         ts =  binning(event.getAttribute('ts'))
@@ -109,9 +176,19 @@ def get_info(file_XML, dados: dict):
             dados[id_patient][ts]["exercise_intensity"] = event.getAttribute('intensity')
             dados[id_patient][ts]["exercise_duration"] = event.getAttribute('duration')
         except:
-            dados[id_patient][ts] = {"exercise_intensity": event.getAttribute('intensity'), "exercise_duration": event.getAttribute('duration'),
-                                     "glucose_level": 0, "medidor": "null"}
+            dados[id_patient][ts] = {"medidor": None, 
+                                     "glucose_level": None,
+                                     "basal": None,
+                                     "bolus": None,
+                                     "bolus_bwz_carb_input": None,
+                                     "meal_type": None,
+                                     "meal_carbs": None,
+                                     "sleep": None,
+                                     "sleep_quality": None,
+                                     "exercise_intensity": event.getAttribute('intensity'),
+                                     "exercise_duration": event.getAttribute('duration')}
 
+    
     return dados
 
 def binning(ts): 
@@ -119,23 +196,24 @@ def binning(ts):
     mim = math.floor(data.minute/5)*5
     return data.replace(minute = mim, second = 0)
     
-lista_XML = get_XMLs(get_xml_root())
+# lista_XML = get_XMLs(get_xml_root())
 
-dados = {}
-for file in lista_XML:
-    dados = get_info(file, dados)
+# dados = {}
+# for file in lista_XML:
+#     dados = get_info(file, dados)
 
-for pacientes in dados:
-    print(f"ID Paciente: {pacientes}")
-    parar = 0 #serve para os lupes não imundar o painel de comando, sera remorido depis
-    for data in dados[pacientes]:
-        print(data)
+# for pacientes in dados:
+#     print(f"ID Paciente: {pacientes}")
+#     for data in dados[pacientes]:
+#         print(data)
 
-        if parar > 4:
-            break
+#         for entrada in dados[pacientes][data]:
+#             print(f"{entrada}: {dados[pacientes][data][entrada]}")
 
-        parar = parar + 1
+root = get_xml_root()
 
-        for entrada in dados[pacientes][data]:
-            print(f"{entrada}: {dados[pacientes][data][entrada]}")
+print(root)
+print(type(root))
+
+
 
